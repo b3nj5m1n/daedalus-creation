ef7e7c42e0d36f0e769aefff5286eb2161968477
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2020-01-01 02:21:05 +0100

    Basic setup

--- daedalus-creation/Program.cs
+++ daedalus-creation/Program.cs
@@ -16,7 +16,7 @@ namespace daedalus_creation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Graphics());
         }
     }
 }
