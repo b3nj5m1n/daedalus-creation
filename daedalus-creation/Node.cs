2bc1da2ccf7443e54ec658427a53625a42c1ce87
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2020-01-01 03:02:48 +0100

    Implemented Node

--- daedalus-creation/Node.cs
+++ daedalus-creation/Node.cs
@@ -8,5 +8,27 @@ namespace daedalus_creation
 {
     class Node
     {
+        // Stores the coordinates in the grid
+        Vector2 coordinates;
+        // Parent nodes are used in certain algorithms
+        Node parent;
+        // Has the node been visited or not, used in certain algorithms
+        bool visited;
+        // Is the node special, will cause it to be drawn in a different color
+        bool special;
+        // Walls sourrounding the node
+        Walls walls;
+        // Potential information that is neccessary for certain algorithms
+        dynamic specific;
+
+        Node(Vector2 Coordinates)
+        {
+            coordinates = Coordinates;
+            parent = null;
+            visited = false;
+            special = false;
+            walls = new Walls();
+            specific = null;
+        }
     }
 }
