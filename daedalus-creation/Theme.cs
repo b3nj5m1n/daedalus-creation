39616a81e0a64eab4ef84948916b56faf5cc5dd1
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2020-01-01 03:16:25 +0100

    Implemented Theme

--- daedalus-creation/Theme.cs
+++ daedalus-creation/Theme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace daedalus_creation
 {
     class Theme
     {
+        int wall_factor = 6;
+        int path_factor = 10;
+        Color color_default = Color.White;
+        Color color_wall = Color.Black;
+        Color color_special = Color.PaleVioletRed;
     }
 }
